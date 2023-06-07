8cfda40b36be625705ff53f89308fd581669fed5
aravind-s1 <121857399+aravind-s1@users.noreply.github.com>
2023-06-07 08:54:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,64 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api",function(req,res){
+  let date = new Date()
+  let timestramp = date.getTime()
+  let json_res = {
+    "unix": timestramp,
+    "utc": date.toUTCString()
+  }
+  res.json(json_res)
+})
+
+app.get("/api/:date", function(req, res) {
+  let params_date = req.params.date;
+  let date = new Date(params_date)
+  if(date.toString()=="Invalid Date")
+    date = new Date(parseInt(params_date))
+  if(date.toString()=="Invalid Date")
+    res.json({
+      "unix":null,
+      "utc":"Invalid Date"
+    })
+  let timestramp = date.getTime()
+  let json_res = {
+    "unix": timestramp,
+    "utc": date.toUTCString()
+  }
+  res.json(json_res)
+
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
